b3c92f4b9a11268bc1ab8d687dc99d6edc856319
YuvalNahon2 <yuval.nahon@campus.technion.ac.il>
2019-06-28 00:58:11 +0300

    new library added

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,24 @@
+val junitVersion = "5.5.0-M1"
+val hamkrestVersion = "1.7.0.0"
+plugins{
+    application
+    id("org.jetbrains.dokka") version "0.9.18"
+}
+
 dependencies {
     compile("il.ac.technion.cs.softwaredesign", "primitive-storage-layer", "1.2")
+
+    compile("io.mockk:mockk:1.9.3")
+    testCompile("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testCompile("com.natpryce:hamkrest:$hamkrestVersion")
+    implementation("com.google.code.gson:gson:2.8.5")
+    runtime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    compile("io.reactivex.rxjava2", "rxjava" , "2.2.9")
+    compile("io.reactivex.rxjava2", "rxkotlin", "2.3.0")
+}
+
+tasks.dokka{
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
 }
