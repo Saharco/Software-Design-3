4922c64c71fb5b0aeff6385cceff1643b770a915
Sahar <saharco26@gmail.com>
2019-08-25 18:40:36 +0300

    testing environment works. currently fails their bad test

--- courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
+++ courseapp-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/CourseAppModule.kt
@@ -1,9 +1,76 @@
 package il.ac.technion.cs.softwaredesign
 
 import com.authzee.kotlinguice4.KotlinModule
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import il.ac.technion.cs.softwaredesign.annotations.DictionaryType
+import il.ac.technion.cs.softwaredesign.annotations.IndexGeneratorForChannelIds
+import il.ac.technion.cs.softwaredesign.constants.EConstants
+import il.ac.technion.cs.softwaredesign.messages.MessageFactory
+import il.ac.technion.cs.softwaredesign.messages.MessageFactoryImpl
+import il.ac.technion.cs.softwaredesign.storage.SecureStorage
+import il.ac.technion.cs.softwaredesign.storage.SecureStorageFactory
+import java.util.concurrent.CompletableFuture
 
 class CourseAppModule : KotlinModule() {
     override fun configure() {
-        TODO("Implement me!")
+        bind<CourseApp>().to<CourseAppImpl>().`in`<Singleton>()
+        bind<CourseAppInitializer>().to<CourseAppInitializerImpl>().`in`<Singleton>()
+        bind<CourseAppStatistics>().to<CourseAppStatisticsImpl>().`in`<Singleton>()
+
+        bind<IStorage>().to<CStorageImpl>().`in`<Singleton>()
+        bind<ICache>().to<CPerpetualICache>()
+        bind<ICacheFactory>().to<CPerpetualCacheFactory>()
+        bind<MessageFactory>().to<MessageFactoryImpl>()
+    }
+
+    @Provides
+    @IndexGeneratorForChannelIds
+    fun provideChannelIdsGenerator(IStorage: IStorage): IUniqueIndexGenerator {
+        return CUniqueIndexGeneratorImpl(IStorage, EConstants.CHANNEL_IDS_GENERATOR.ordinal)
+    }
+
+    @Singleton
+    @Provides
+    fun provideSecureStorage(secureStorageFactory: SecureStorageFactory): CompletableFuture<SecureStorage> {
+        return secureStorageFactory.open("main".toBytes())
+    }
+
+    fun getCompArray(): ArrayList<(String, String) -> Int> {
+        val comp1 = fun(a: String, b: String): Int { return a.toInt() - b.toInt() }
+        val comp2 = fun(a: String, b: String): Int { return b.toInt() - a.toInt() }
+        return arrayListOf(comp1, comp2)
+    }
+
+    @Provides
+    @Singleton
+    @DictionaryType("channelMsgs")
+    fun providesChannelMsgs(IStorage: IStorage): IDictionary {
+        val pointerGenerator = CUniqueIndexGeneratorImpl(IStorage, EConstants.CHANNEL_MESSAGES_AVL.ordinal)
+        return CAvlTree(IStorage,pointerGenerator, EConstants.CHANNEL_MESSAGES_AVL.ordinal, getCompArray())
+    }
+
+    @Provides
+    @Singleton
+    @DictionaryType("channelActiveUsers")
+    fun providesChannelActiveUsersDictionary(IStorage:IStorage): IDictionary {
+        val pointerGenerator = CUniqueIndexGeneratorImpl(IStorage, EConstants.CHANNEL_ACTIVE_USERS_AVL.ordinal)
+        return CAvlTree(IStorage,pointerGenerator, EConstants.CHANNEL_ACTIVE_USERS_AVL.ordinal, getCompArray())
+    }
+
+    @Provides
+    @Singleton
+    @DictionaryType("channelUsers")
+    fun providesChannelTotalUsersDictionary(IStorage:IStorage):  IDictionary {
+        val pointerGenerator = CUniqueIndexGeneratorImpl(IStorage, EConstants.CHANNEL_USERS_AVL.ordinal)
+        return CAvlTree(IStorage,pointerGenerator, EConstants.CHANNEL_USERS_AVL.ordinal, getCompArray())
+    }
+
+    @Provides
+    @Singleton
+    @DictionaryType("userChannels")
+    fun providesUserChannelsDictionary(IStorage:IStorage):  IDictionary {
+        val pointerGenerator = CUniqueIndexGeneratorImpl(IStorage, EConstants.USER_CHANNELS_AVL.ordinal)
+        return CAvlTree(IStorage,pointerGenerator, EConstants.USER_CHANNELS_AVL.ordinal, getCompArray())
     }
 }
--- courseapp-test/build.gradle.kts
+++ courseapp-test/build.gradle.kts
@@ -1,3 +1,4 @@
+import java.time.Duration
 plugins {
 }
 
@@ -5,7 +6,6 @@ val junitVersion: String? by extra
 val hamkrestVersion: String? by extra
 val guiceVersion: String? by extra
 val kotlinGuiceVersion: String? by extra
-val mockkVersion: String? by extra
 
 dependencies {
     compile(project(":library"))
@@ -17,6 +17,55 @@ dependencies {
     testCompile("com.google.inject", "guice", guiceVersion)
     testCompile("com.authzee.kotlinguice4", "kotlin-guice", kotlinGuiceVersion)
 
-    testImplementation("io.mockk", "mockk", mockkVersion)
+    testImplementation("io.mockk", "mockk", "1.9.3")
+
+    runtime("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
 }
+
+tasks.test{
+    useJUnitPlatform()
+
+    // Make sure tests don't take over 40 minutes
+    timeout.set(Duration.ofMinutes(40))
+    reports{
+        junitXml.isEnabled = false
+        html.isEnabled = true
+
+        //html.destination = File("path\\to\\dir\\destination")
+    }
+
+    testLogging{
+        val csvFile=File("C:\\Users\\Sahar\\Desktop\\Code\\Software-Design\\Tests\\HW3\\summary.csv")
+        var toprow ="ClassName,TestName,Result,Duration(ms)\n"
+
+
+        var content = ""
+        addTestListener(object : TestListener {
+            override fun beforeSuite(suite: TestDescriptor) {
+                if(suite.parent == null){
+                    csvFile.appendText(toprow)
+                }
+            }
+            override fun beforeTest(testDescriptor: TestDescriptor) {}
+            override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+                content += testDescriptor.getClassName()+","+
+                        testDescriptor.getName()+","+
+                        result.resultType.toString()+","+
+                        (result.endTime-result.startTime).toString()+"\n"
+
+            }
+            override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                if(suite.parent == null){
+                    println("Logging to csv at "+csvFile.absolutePath)
+                    csvFile.appendText(content)
+                    content=""
+                }
+
+            }
+        })
+    }
+
+}
+
+
 
